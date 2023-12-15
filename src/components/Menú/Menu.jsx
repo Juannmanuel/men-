a5661deb2fdc4296b942ca0e6e3ff591047d0646
Juan Manuel <losredondos551@gmail.com>
2023-12-15 09:23:43 -0300

    Maquetado menu

--- src/components/Menú/Menu.jsx
+++ src/components/Menú/Menu.jsx
@@ -1,13 +1,21 @@
-import style from "./Menu.module.css"
+import style from "./Menu.module.css";
 
-function Menu(){
-  const titles = ["Cafetería", "Bebidas", "Comida", "Menú del Día", "Menú Infantil"  ]
+function Menu() {
+  const titles = [
+    "https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg",
+    "https://www.themealdb.com/images/media/meals/pkopc31683207947.jpg",
+    "https://www.themealdb.com/images/media/meals/z0ageb1583189517.jpg",
+    "https://www.themealdb.com/images/media/meals/vtqxtu1511784197.jpg",
+    "https://www.themealdb.com/images/media/meals/020z181619788503.jpg",
+  ];
 
-    return(
-        <div className={style.menu_main}>
-        {titles.map((item) => <div className={style.content_menu}>{item}</div>) }    
-        </div>
-    )
+  return (
+    <div className={style.menu_main}>
+      {titles.map((item) => (
+        <div className={style.content_menu}><img src={item} alt="Comida" /></div>
+      ))}
+    </div>
+  );
 }
 
-export default Menu
+export default Menu;
